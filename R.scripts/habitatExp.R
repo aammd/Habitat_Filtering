f19d7711db1ae068a1f93d15edd94f2d6247375a
Andrew <a.a.m.macdonald@gmail.com>
2014-08-22 11:41:42 -0700

    no more defining functions over here

--- R.scripts/habitatExp.R
+++ R.scripts/habitatExp.R
@@ -14,38 +14,8 @@ library(vegan)
 library(pander)
 
 source("R.scripts/read_clean_data.R")
-
+source("R.scripts/HabitatSizeFunctions.R")
 ## Insects: PERMANOVA -----------------------------------------------------
-
-TaxaTimeSelector <- function(.blocks = blocks, .bromeliad = bromeliad, 
-                           .taxa = insects_renamed,
-                           sampletime = "final") {
-  ## select blocks
-  .blocks %>%
-    filter(experiment == "threespp") %>%
-    select(Block = block) %>%
-    ## merge to bromeliad
-    left_join(.bromeliad %>% select(Brom, Block, species)) %>%
-    # and add the animals
-    left_join(.taxa %>%
-                filter(sampling == sampletime) %>%
-                spread(key = Spp, abundance, fill = 0) %>%
-                mutate(Brom = bromeliad) %>%
-                select(-bromeliad)
-    ) %>%
-    # set up a list object a la mvabund
-    (l(data ~ {
-      list(factors = data %>% 
-             select(Block, species),
-           insects = data %>% 
-             select(-Block, -Brom, -species, -sampling))
-    }))
- 
-}
-
-AdonisData <- function(Data, ...){  
-  adonis(Data[[2]] ~ species, data = Data[[1]], strata = Data[[1]]$Block, ...)
-}
 
 # final insects
 insectfinal <- AdonisData(TaxaTimeSelector())
@@ -73,37 +43,12 @@ zf <- AdonisData(TaxaTimeSelector(.taxa = zoop_combined,
 
 # Bacteria from threespp --------------------------------------------------
 
-BacteriaTimeSelector <- function(.bacteria_list = bacteria_list,
-                                 sampletime = c("initial","final")){
-  
-  plyr::llply(bacteria_list,function(BACTERIA){
-    ## select blocks
-    bact_data <- blocks %>%
-      filter(experiment=="threespp") %>%
-      select(Block=block) %>%
-      ## merge to bromeliad
-      left_join(bromeliad) %>%
-      mutate(bromeliad=Brom) %>%
-      select(-Brom) %>%
-      # and add the animals
-      ## note that for bacteria, they are joined in the opposite direction!
-      left_join(BACTERIA,
-                .         # this dot essentially makes this a "right join".
-      ) %>% 
-      ## the following is a kludgy way to remove NAs.
-      (l(data ~ filter(data,!is.na(rowSums(select(data,starts_with("X"))))))) %>%
-      # and now we filter to keep initial, final or both:
-      filter(sampling %in% sampletime) %>%
-      # set up a list object a la mvabund
-      (l(data ~ {
-        list(factors=data %>% select(Block,species,sampling),
-             bacts=data %>% select(starts_with("X")))
-      }
-      ))
-  })
-}
-  
+
   #bacteria_adonis_ready$Eccleson$bacts
+
+
+lapply(bacteria_list, BacteriaTimeSelector, sampletime = c("initial","final"))
+
 
 bacteria_adonis_ready <- BacteriaTimeSelector()
 lapply(bacteria_adonis_ready, function(Data, ...){  
