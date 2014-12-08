07fdbe173564a9b17d883d498f7e7c753e747096
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-12-08 09:58:09 -0800

    make to read and write inverts.csv

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ clean:
 
 .PHONY: all clean
 
-data/inverts.csv 
+data/inverts.csv : R.scripts/inverts.R raw-data/insect.communities.table.txt raw-data/insectnames.csv
+	cd $(<D); Rscript $(<F)
--- R.scripts/inverts.R
+++ R.scripts/inverts.R
@@ -3,17 +3,19 @@
 
 library(dplyr)
 
-insects <-   read.table("../data/insect.communities.table.txt",
-                        header=TRUE,comment.char="#",stringsAsFactors=FALSE) %>% tbl_df()
+insects <-   read.table("../raw-data/insect.communities.table.txt",
+                        header=TRUE, comment.char="#", stringsAsFactors=FALSE) %>% tbl_df()
 
-insectnames <- read.csv(file = "../data/insectnames.csv") %>% tbl_df()
+insectnames <- read.csv(file = "../raw-data/insectnames.csv", stringsAsFactors=FALSE) %>% tbl_df()
 
 # renaming insects taxa ---------------------------------------------------
 
 ## correct variable spellings
-insects_renamed <- insects %>%
+insects %>%
   left_join(insectnames) %>%
   select(sampling, bromeliad, Spp = sp_name, abundance) %>%
   group_by(sampling, bromeliad, Spp) %>%
-  summarise(abundance = sum(abundance))
+  summarise(abundance = sum(abundance)) %>%
+  as.data.frame %>%
+  write.csv(file = "../data/inverts.csv", row.names = FALSE)
 
