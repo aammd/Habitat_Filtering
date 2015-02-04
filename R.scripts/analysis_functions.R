d21bff2afaf86ea23c86a1571e5ab6dcdb112c6b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-04 11:43:34 -0800

    roxygen comments. made the filter a variable (hopefully useful later for 2nd experiment.)

--- R.scripts/analysis_functions.R
+++ R.scripts/analysis_functions.R
@@ -1,17 +1,24 @@
 ## Functions for analyzing the R data
-## 
+## Andrew MacDonald 2015
 
-
+#' get all the insects into a list
+#' 
+#' binds the blocks to the insects, before filtering, so that we can be sure
+#' that environmental variables and species data line up
+#' 
+#' @param .blocks the blocks data.frame
+#' @param .bromeliad the bromeliad data.frame
+#' @param .taxa the insects_renamed object (insects after cleaning the names)
+#' @param sampletime which sample to analyze. Defaults to "final"
+#' @param which_exp which experiment are we looking at here?
 insect_manyglm <- function(.blocks = blocks,
                            .bromeliad = bromeliad, 
-                           .taxa = insects_renamed, 
-                           run_interaction_model = FALSE, 
-                           sampletime = "final", 
-                           glm_family = "negative.binomial",
-                           organisms = "insects") {
+                           .taxa = insects_renamed,
+                           sampletime = "final",
+                           which_exp = "threespp") {
   ## select blocks
   insect_data <- .blocks %>%
-    filter(experiment == "threespp") %>%
+    filter(experiment == which_exp) %>%
     select(Block = block) %>%
     ## merge to bromeliad
     left_join(.bromeliad %>% select(Brom, Block, species)) %>%
