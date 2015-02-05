80bb7b8e368a3c6cb34add5e8fb5c242741d5fb2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-05 10:37:50 -0800

    remove that which is now a function

--- R.scripts/habitatExp.R
+++ R.scripts/habitatExp.R
@@ -11,29 +11,7 @@ insect_manyglm <- function(.blocks = blocks,
                            sampletime = "final", 
                            glm_family = "negative.binomial",
                            organisms = "insects") {
-  ## select blocks
-  insect_data <- .blocks %>%
-    filter(experiment == "threespp") %>%
-    select(Block = block) %>%
-    ## merge to bromeliad
-    left_join(.bromeliad %>% select(Brom, Block, species)) %>%
-    # and add the animals
-    left_join(.taxa %>%
-                filter(sampling == sampletime) %>%
-                spread(key = Spp, abundance, fill = 0) %>%
-                mutate(Brom = bromeliad) %>%
-                select(-bromeliad)
-    ) %>%
-    # set up a list object a la mvabund
-    {
-      list(factors = . %>% 
-             select(Block, species) %>% 
-             as.matrix,
-           insects = . %>% 
-             select(-Block, -Brom, -species, -sampling) %>% 
-             as.matrix)
-    }
-    
+
   
   ## call mvabund on responses
   insectresponses <- insect_data %>% 
