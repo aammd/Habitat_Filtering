fbfa0bcc0a2a59bbc0571bdf5aea0fe7fb1217e0
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-07 13:27:04 -0800

    moved over analysis functions

--- R.scripts/data_organizing.R
+++ R.scripts/data_organizing.R
@@ -44,14 +44,6 @@ TaxaTimeSelector <- function(.blocks = blocks,
 
 
 
-## runs `adonis` on the output of TaxaTimeSelector. Knows
-## where to find species abundances.  Also defaults to
-## setting strata as Block. For bacteria (which have to be
-## analyzed separately) just set .strata to NULL
-
-AdonisData <- function(Data, .strata = Data[[1]]$Block, ...){  
-  adonis(Data[[2]] ~ species, data = Data[[1]], strata = .strata, ...)
-}
 
 ## wrapper for filter that removes samples that failed to
 ## run (ie were NA in the data)
@@ -129,15 +121,6 @@ InsectZooBactAbds <- function(sampletime = "initial", .insectlist = insectlist,
 }
 
 
-## ... passes refinements of the `strata` argument to AdonisData, otherwise does nothing
-## add a "testclass" argument, or perhaps a "logical test" arguement:
-## if I go with testclass, must have a new class. 
-CommunityAdonis <- function(data_list, testclass, fun, ...){  
-  if(inherits(data_list, testclass)) {
-    fun(data_list)
-  } else {
-    lapply(data_list, CommunityAdonis, testclass, fun, ...)
-  }
-}
+
 
 
--- R.scripts/run_analyses.R
+++ R.scripts/run_analyses.R
@@ -1,6 +1,24 @@
 ## running the analyses
 
+## runs `adonis` on the output of TaxaTimeSelector. Knows
+## where to find species abundances.  Also defaults to
+## setting strata as Block. For bacteria (which have to be
+## analyzed separately) just set .strata to NULL
 
+AdonisData <- function(Data, .strata = Data[[1]]$Block, ...){  
+  adonis(Data[[2]] ~ species, data = Data[[1]], strata = .strata, ...)
+}
+
+## ... passes refinements of the `strata` argument to AdonisData, otherwise does nothing
+## add a "testclass" argument, or perhaps a "logical test" arguement:
+## if I go with testclass, must have a new class. 
+CommunityAdonis <- function(data_list, testclass, fun, ...){  
+  if(inherits(data_list, testclass)) {
+    fun(data_list)
+  } else {
+    lapply(data_list, CommunityAdonis, testclass, fun, ...)
+  }
+}
 
 runadonis <- partial(CommunityAdonis, testclass = "ExpAbd", 
                      fun = AdonisData, .strata = NULL)
