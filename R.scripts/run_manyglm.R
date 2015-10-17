81d3eba72d207d615a5d3a6a58ee28678560b15b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-10-17 14:40:24 -0700

    move manyglm function to its own script

---
+++ R.scripts/run_manyglm.R
@@ -0,0 +1,93 @@
+## run and report the manyglm results
+# insects in threespp experiment ------------------------------------------
+
+insect_manyglm <- function(.blocks = blocks, .bromeliad = bromeliad, 
+                           .taxa = insects_renamed, 
+                           run_interaction_model = FALSE, 
+                           sampletime = "final", glm_family = "negative.binomial",
+                           organisms = "insects") {
+  ## select blocks
+  insect_data <- .blocks %>%
+    filter(experiment == "threespp") %>%
+    select(Block = block) %>%
+    ## merge to bromeliad
+    left_join(.bromeliad %>% select(Brom, Block, species)) %>%
+    # and add the animals
+    left_join(.taxa %>%
+                filter(sampling == sampletime) %>%
+                spread(key = Spp, abundance, fill = 0) %>%
+                mutate(Brom = bromeliad) %>%
+                select(-bromeliad)
+    ) %>%
+    # set up a list object a la mvabund
+    l(data -> {
+      list(factors = data %>% 
+             select(Block, species) %>% 
+             as.matrix,
+           insects = data %>% 
+             select(-Block, -Brom, -species, -sampling) %>% 
+             as.matrix)}
+    )
+  
+  ## call mvabund on responses
+  insectresponses <- insect_data %>% 
+    extract2("insects") %>%
+    mvabund
+  
+  ## run glm
+  insect_glm_interact <- insect_data %>% 
+    extract2("factors") %>% 
+    data.frame %>% 
+    manyglm(insectresponses ~ Block * species, data = ., family = glm_family) 
+  
+  ## name the model output
+  model_name <- paste0(organisms, "_interaction_summary_", sampletime, "_", glm_family, ".Rdata")
+  
+  # summary gives overall fit
+  if (run_interaction_model) {
+    insect_interact_summary <- insect_glm_interact %>% 
+      summary(resamp="residual")
+    save(insect_interact_summary, file = model_name)
+    message(paste("I just created the file",model_name))
+  } else {
+    load(model_name)
+  }
+  
+  ## name the anova output
+  anova_name <- paste0(organisms, "_interaction_anova_", sampletime, "_", glm_family, ".Rdata")
+  
+  # anova gives us values for each animal
+  if (run_interaction_model) {
+    insect_interact_anova  <- insect_glm_interact %>% 
+      anova(resamp="perm.resid", p.uni="adjusted", show.time="all")
+    save(insect_interact_anova, file = anova_name)
+    message(paste("I just created the file", anova_name))
+  } else {
+    load(anova_name)
+  }
+  
+  insect_statistic <- insect_interact_anova %>%
+    extract2("uni.test") %>% 
+    t %>% 
+    data.frame %>% 
+    select(-X.Intercept.,
+           Block_wald=Block,
+           species_wald=species) %>%
+    l(df -> data.frame(spp=rownames(df),df)) %>%
+    set_rownames(NULL)
+  
+  insect_sig <- insect_interact_anova %>%
+    extract2("uni.p") %>%
+    t %>% 
+    data.frame %>% 
+    select(-X.Intercept.,
+           Block_p=Block,
+           species_p=species) %>%
+    l(df -> data.frame(spp=rownames(df),df)) %>%
+    set_rownames(NULL)
+  
+  list(plotting_data = left_join(insect_sig, insect_statistic),
+       manyglm_summary = insect_interact_summary,
+       manyglm_anova = insect_interact_anova,
+       manyglm = insect_glm_interact)
+}
