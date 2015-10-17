7fdce9eea8d27ac4d459e83eeb6626bde89dcbad
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-10-17 12:16:20 -0700

    corrects error

--- create_all.sh
+++ create_all.sh
@@ -7,7 +7,7 @@ cp /home/andrew/Documents/reference/Mendeley_reference_lists/@MS_habitat_size_or
 cat ./MS/Rcitations.bibtex >> ./MS/orgsize.bib
 
 ## copy over citation format from the correct source.
-cp .../../reference/styles/the-american-naturalist.csl ./MS/formatting/
+cp ../../reference/styles/the-american-naturalist.csl ./MS/formatting/
 
 ## finally make everthing
 remake
