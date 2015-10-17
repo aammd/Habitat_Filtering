9dc7b21bd10d165d1fc8d1754c31aed194d5b9a1
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-10-17 15:16:04 -0700

    remove arguements already handled by TaxaTimeSelector

--- R.scripts/run_manyglm.R
+++ R.scripts/run_manyglm.R
@@ -6,32 +6,10 @@ insect_manyglm <- function(.blocks = blocks, .bromeliad = bromeliad,
                            run_interaction_model = FALSE, 
                            sampletime = "final", glm_family = "negative.binomial",
                            organisms = "insects") {
-  ## select blocks
-  insect_data <- .blocks %>%
-    filter(experiment == "threespp") %>%
-    select(Block = block) %>%
-    ## merge to bromeliad
-    left_join(.bromeliad %>% select(Brom, Block, species)) %>%
-    # and add the animals
-    left_join(.taxa %>%
-                filter(sampling == sampletime) %>%
-                spread(key = Spp, abundance, fill = 0) %>%
-                mutate(Brom = bromeliad) %>%
-                select(-bromeliad)
-    ) %>%
-    # set up a list object a la mvabund
-    l(data -> {
-      list(factors = data %>% 
-             select(Block, species) %>% 
-             as.matrix,
-           insects = data %>% 
-             select(-Block, -Brom, -species, -sampling) %>% 
-             as.matrix)}
-    )
-  
+
   ## call mvabund on responses
   insectresponses <- insect_data %>% 
-    extract2("insects") %>%
+    extract2("taxa") %>%
     mvabund
   
   ## run glm
