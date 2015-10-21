600b9caeb3c82c33b62b4e3ad1e630e1db1d5a99
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-10-21 04:32:46 -0700

    removing stuff as it gets abstracted into fucntions

--- R.scripts/linearmodels.R
+++ R.scripts/linearmodels.R
@@ -5,7 +5,7 @@ disp_difs_long  %>%
 disp_aov_err <- disp_difs_long  %>% 
   aov(value ~ taxa * timing + Error(block), data = .)
 
-summary.lm(disp_aov_err)
+stargazer::stargazer(disp_aov_err)
 
 library(lme4)
 disp_aov <- lmer(value ~ taxa * timing + (1|block), data = disp_difs_long)
@@ -14,11 +14,13 @@ stargazer::stargazer(disp_aov)
 summary(disp_aov)
 
 
-library(nlme)
-mixedmod <- lme(value ~ taxa*timing, random = ~1|block, data = disp_difs_long)
-summary(mixedmod)
-stargazer::stargazer(mixedmod)
+sum_mixed <- summary(mixedmod)
 
+str(aov_mixed)
+aov_mixed$`p-value`
+
+# stargazer::stargazer(mixedmod)
+# doesn't work!
 head(disp_difs_long)
 disp_difs_long %>% 
   spread(timing, value) %>% 
@@ -26,8 +28,3 @@ disp_difs_long %>%
   lm(percent_dec ~ taxa, data = .) %>% 
   summary
 
-
-disp_difs_long %>% 
-  ggplot(aes(x = timing, y = value, group = block)) +
-  geom_line() +
-  facet_wrap(~taxa)
