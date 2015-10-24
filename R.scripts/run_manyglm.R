f2b10156e72b9d2eec8177a8c9586f84818c4ecd
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-10-23 17:13:03 -0700

    move all glm related things to same place

--- R.scripts/run_manyglm.R
+++ R.scripts/run_manyglm.R
@@ -34,7 +34,8 @@ get_model_stats <- function(anova_output){
     data.frame %>% 
     select(-X.Intercept.,
            Block_wald = Block,
-           species_wald = species) %>%
+           species_wald = species,
+           Block.species_wald = Block.species) %>%
     add_rownames()
   
   insect_sig <- anova_output %>%
@@ -43,7 +44,8 @@ get_model_stats <- function(anova_output){
     data.frame %>% 
     select(-X.Intercept.,
            Block_p = Block,
-           species_p = species) %>%
+           species_p = species,
+           Block.species_p = Block.species) %>%
     add_rownames() %>%
     set_rownames(NULL)
   
@@ -64,4 +66,50 @@ run_manyglm_add <- function(data_list, glm_family = "negative.binomial"){
     extract2("factors") %>% 
     data.frame %>% 
     manyglm(responses ~ Block + species, data = ., family = glm_family)
+}
+
+## fits the block effect only
+## function to run block and species:block
+run_manyglm_no_main_sp <- function(data_list, glm_family = "negative.binomial"){  
+  #' call mvabund on responses
+  responses <- data_list %>% 
+    extract2("taxa") %>% 
+    as.matrix
+  # browser()
+  ## run glm
+  data_list %>% 
+    extract2("factors") %>% 
+    data.frame %>% 
+    manyglm(responses ~ Block, data = ., family = glm_family)
+}
+
+### null model
+run_manyglm_null <- function(data_list, glm_family = "negative.binomial"){  
+  #' call mvabund on responses
+  responses <- data_list %>% 
+    extract2("taxa") %>% 
+    as.matrix
+  # browser()
+  ## run glm
+  data_list %>% 
+    extract2("factors") %>% 
+    data.frame %>% 
+    manyglm(responses ~ 1, data = ., family = glm_family)
+}
+
+
+
+## info extraction functions
+## calculate percent deviance
+manyglm_get_table <- function(.manyglm_anova_result){
+  devtable <- .manyglm_anova_result$table
+  add_rownames(devtable, var = "term")
+}
+
+percent_dev_sp <- function(.manyglm_df){
+  ## find species
+  whersp <- grepl("sp", .manyglm_df$term)
+  tot <- sum(.manyglm_df$Dev, na.rm = TRUE)
+  sp <- sum(.manyglm_df$Dev[whersp], na.rm = TRUE)
+  (sp/tot)*100
 }
