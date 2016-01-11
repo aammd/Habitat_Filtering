b8fab38cd9d36dda1a11e59947e9b35e92329a3b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-01-10 23:27:21 -0800

    move reference creation to a separate step

--- create_all.sh
+++ create_all.sh
@@ -4,14 +4,9 @@
 ## copy over the theme from the first chapter:
 cp ../01_PredPhylo/R.scripts/FIGURE_themes.R ./R.scripts/
 
-## obtain the bibliography from Mendeley
-cp /home/andrew/Documents/reference/Mendeley_reference_lists/04_OrgSizeHabitat.bib ./MS/orgsize.bib
-
-## append to the end the R package citations
-cat ./MS/Rcitations.bibtex >> ./MS/orgsize.bib
+bash create_refs.sh
 
 ## copy over citation format from the correct source.
 cp ../../reference/styles/the-american-naturalist.csl ./MS/formatting/
 
-## finally make everthing
 remake
