62b3c5ff7f21ddbffc7def36b69515419851f35b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-01-25 03:12:41 -0800

    trim these down, now that scripted means exists of making everything pdf

--- R.scripts/render_convenience.R
+++ R.scripts/render_convenience.R
@@ -1,23 +1,17 @@
 ### make the png file names into pdfs
 
 png_to_pdf_filename <- function(md_file){
-  "sed -i 's/r2_plot.png/r2_plot.pdf/g' %s" %>% 
+  "sed -i 's/.png)/.pdf)/g' %s" %>% 
     sprintf(md_file) %>% 
     system()
-  
-  "sed -i 's/individual_responses.png/individual_responses.pdf/g' %s" %>% 
-    sprintf(md_file) %>% 
-    system()
+
 }
 
 pdf_to_png_filename <- function(md_file){
-  "sed -i 's/r2_plot.pdf/r2_plot.png/g' %s" %>% 
+  "sed -i 's/.pdf)/.png)/g' %s" %>% 
     sprintf(md_file) %>% 
     system()
-  
-  "sed -i 's/individual_responses.pdf/individual_responses.png/g' %s" %>% 
-    sprintf(md_file) %>% 
-    system()
+
 }
 
 render_pdf <- function(filename, ...){
