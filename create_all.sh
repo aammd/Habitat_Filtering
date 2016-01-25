9a3c976d1fe22dc324028cbfe86eb00581b33570
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-01-25 03:13:16 -0800

    script :joy: to generate pdf from png

--- create_all.sh
+++ create_all.sh
@@ -6,6 +6,8 @@ cp ../01_PredPhylo/R.scripts/FIGURE_themes.R ./R.scripts/
 
 bash create_refs.sh
 
+bash create_illustration.sh
+
 ## copy over citation format from the correct source.
 cp ../../reference/styles/the-american-naturalist.csl ./MS/formatting/
 
---
+++ create_illustration.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+inkscape --file=photos/hypotheses.svg --export-area-drawing --without-gui --export-pdf=photos/hypotheses_illustration.pdf
+
+inkscape --file=photos/design.svg --export-area-drawing --without-gui --export-pdf=photos/design_illustration.pdf
+
