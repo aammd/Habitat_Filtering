7f81f3b1cc43400b9e45f464d462df2a55b6d134
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2018-03-29 14:58:57 +0200

    Edit reference .sh script No longer using mendeley to manage

--- create_refs.sh
+++ create_refs.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-		
-## obtain the bibliography from Mendeley
-cp /home/andrew/Documents/reference/Mendeley_reference_lists/04_OrgSizeHabitat.bib ./MS/orgsize.bib
-
-## append to the end the R package citations
-cat ./MS/Rcitations.bibtex >> ./MS/orgsize.bib
 
 ## copy over citation format from the correct source.
 cp ../../reference/styles/the-american-naturalist.csl ./MS/formatting/
